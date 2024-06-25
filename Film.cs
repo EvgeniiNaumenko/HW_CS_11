e3e001db49b2bed4ba45f93a292a50ba294557d6
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-06-25 21:18:14 +0300

    first commit

---
+++ Film.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11_19_06_2024
+{
+    internal class Film
+    {
+        public string Title { get; set; }
+        public string Studio { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; } 
+        public int Year { get; set; }
+        public Film (string title, string studio, string genre, int duration,int year)
+        {
+            Title = title;
+            Studio = studio;
+            Genre = genre;
+            Duration = duration;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}\nStudio: {Studio}\nGenre: {Genre}\nDuration: {Duration}\nYear: {Year}";
+        }
+        ~Film()
+        {
+            Console.WriteLine($"Film '{Title}' removed.");
+        }
+
+    }
+}
---
+++ Performance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11_19_06_2024
+{
+    internal class Performance: IDisposable
+    {
+        public string Name {  get; set; }
+        public string TheaterName { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public List<String> CastList;
+
+        public Performance() 
+        {
+            Name = "NoName";
+            TheaterName = "NoName";
+            Genre = "NoGenre";
+            CastList = new List<string>();
+        }
+        public Performance(string name, string theaterName, string genre, int duration)
+        {
+            Name = name;
+            TheaterName = theaterName;
+            Genre = genre;
+            Duration = duration;
+            CastList = new List<string>();
+        }
+        public void AddCast (string name)
+        {
+            CastList.Add(name);
+        }
+        public void RemoveCast (string name)
+        {
+            CastList.Remove(name);
+        }
+        public void Dispose()
+        {
+            CastList.Clear();
+            Console.WriteLine("Type clear");
+        }
+        ~Performance()
+        {
+            Console.WriteLine($"Performance '{Name}' removed.");
+        }
+        public override string ToString()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"TheaterName: {TheaterName}");
+            Console.WriteLine($"Genre: {Genre}");
+            Console.WriteLine($"Duration: {Duration}");
+            Console.WriteLine("Cast list:");
+            foreach ( var cast in CastList )
+            {
+                Console.WriteLine(cast);
+            }
+            return "";
+        }
+    }
+}
